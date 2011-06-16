3a4afbbb5db2cd9eae8a0bdf7f4be2cdc36f502b
Vitta <Vitta@fb370eea-3af6-4597-97f7-f7400a59c12a>
2011-06-16 16:32:40 +0000

    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/applications/direct/trunk@203303 fb370eea-3af6-4597-97f7-f7400a59c12a

--- src/web/scripts/tiny_mce/plugins/wordcount/editor_plugin_src.js
+++ src/web/scripts/tiny_mce/plugins/wordcount/editor_plugin_src.js
@@ -4,22 +4,29 @@
  * Copyright 2009, Moxiecode Systems AB
  * Released under LGPL License.
  *
+ * version 1.1
+ * - Add more conditions for triggering the counting.
+ * - The interval of two counting is set to 0ms instead of 2000ms.
+ * - Numbers are also counted as a word.
  * License: http://tinymce.moxiecode.com/license
  * Contributing: http://tinymce.moxiecode.com/contributing
+* @author TCSDEVELOPER
+ * @version 1.1(Direct Improvements Assembly Release 2)
  */
 
 (function() {
-    tinymce.create('tinymce.plugins.WordCount', {
+	tinymce.create('tinymce.plugins.WordCount', {
 		block : 0,
 		id : null,
 		countre : null,
 		cleanre : null,
-
+		count : 0,
+		
 		init : function(ed, url) {
 			var t = this, last = 0;
 
-			t.countre = ed.getParam('wordcount_countregex', /\S\s+/g);
-			t.cleanre = ed.getParam('wordcount_cleanregex', /[0-9.(),;:!?%#$¿'"_+=\\/-]*/g);
+			t.countre = ed.getParam('wordcount_countregex', /[\w\u2019\'-]+/g); // u2019 == &rsquo;
+			t.cleanre = ed.getParam('wordcount_cleanregex', /[.(),;:!?%#$?\'\"_+=\\\/-]*/g);
 			t.id = ed.id + '-word-count';
 
 			ed.onPostRender.add(function(ed, cm) {
@@ -32,11 +39,12 @@
 
 					if (row)
 						tinymce.DOM.add(row.parentNode, 'div', {'style': 'float: right'}, ed.getLang('wordcount.words', 'Words: ') + '<span id="' + t.id + '">0</span>');
-				} else
+				} else {
 					tinymce.DOM.add(id, 'span', {}, '<span id="' + t.id + '">0</span>');
+				}
 			});
 
-            ed.onInit.add(function(ed) {
+			ed.onInit.add(function(ed) {
 				ed.selection.onSetContent.add(function() {
 					t._count(ed);
 				});
@@ -49,18 +57,44 @@
 			});
 
 			ed.onKeyUp.add(function(ed, e) {
-				if (e.keyCode == last)
+				if (e.keyCode == last && last != 8 && last != 46)
 					return;
-
-				if (13 == e.keyCode || 8 == last || 46 == last)
+				
+				//Add more trigger conditions
+				if (count == 0 || 13 == last || 32 == last || 8 == e.keyCode || 46 == e.keyCode)
 					t._count(ed);
 
 				last = e.keyCode;
 			});
+			
+			ed.onPaste.add(function(ed) {
+				t._count(ed);
+			});
+		},
+
+		_getCount : function(ed) {
+			var tc = 0;
+			var tx = ed.getContent({ format: 'raw' });
+
+			if (tx) {
+					tx = tx.replace(/\.\.\./g, ' '); // convert ellipses to spaces
+					tx = tx.replace(/<.[^<>]*?>/g, ' ').replace(/&nbsp;|&#160;/gi, ' '); // remove html tags and space chars
+
+					// deal with html entities
+					tx = tx.replace(/(\w+)(&.+?;)+(\w+)/, "$1$3").replace(/&.+?;/g, ' ');
+					tx = tx.replace(this.cleanre, ''); // remove punctuation
+
+					var wordArray = tx.match(this.countre);
+					if (wordArray) {
+							tc = wordArray.length;
+					}
+			}
+
+			return tc;
 		},
 
 		_count : function(ed) {
-			var t = this, tc = 0;
+			var t = this;
 
 			// Keep multiple calls from happening at the same time
 			if (t.block)
@@ -69,21 +103,15 @@
 			t.block = 1;
 
 			setTimeout(function() {
-				var tx = ed.getContent({format : 'raw'});
-
-				if (tx) {
-					tx = tx.replace(/<.[^<>]*?>/g, ' ').replace(/&nbsp;|&#160;/gi, ' '); // remove html tags and space chars
-					tx = tx.replace(t.cleanre, ''); // remove numbers and punctuation
-					tx.replace(t.countre, function() {tc++;}); // count the words
-				}
-
-				tinymce.DOM.setHTML(t.id, tc.toString());
-
-				setTimeout(function() {t.block = 0;}, 2000);
+					var tc = t._getCount(ed);
+					count = tc;
+					tinymce.DOM.setHTML(t.id, tc.toString());
+					// Change to trigger next event immediately.
+					setTimeout(function() {t.block = 0;}, 0);
 			}, 1);
 		},
 
-        getInfo: function() {
+		getInfo: function() {
 			return {
 				longname : 'Word Count plugin',
 				author : 'Moxiecode Systems AB',
@@ -91,8 +119,8 @@
 				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/wordcount',
 				version : tinymce.majorVersion + "." + tinymce.minorVersion
 			};
-        }
-    });
+		}
+	});
 
-    tinymce.PluginManager.add('wordcount', tinymce.plugins.WordCount);
+	tinymce.PluginManager.add('wordcount', tinymce.plugins.WordCount);
 })();
