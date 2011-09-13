04df1d90eb04454077e2ccb6b99d4221de8588c3
TonyJ <TonyJ@fb370eea-3af6-4597-97f7-f7400a59c12a>
2011-09-13 19:43:00 +0000

    updated feed URL to point at www.topcoder.com/wp-admin
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/applications/direct/trunk@207395 fb370eea-3af6-4597-97f7-f7400a59c12a

--- src/web/scripts/FeedLoader.js
+++ src/web/scripts/FeedLoader.js
@@ -38,7 +38,7 @@ function loadHomePageFeeds() {
 
     // NOTE: The following block is commented out for testing/review purposes only. It should be
     // uncommented when deploying to Production environment
-    loadFeed("https://www.topcoder.com/homeblogfeed/", "/scripts/DirectNewsTemplate.txt", "newsColumn", 
+    loadFeed("https://www.topcoder.com/feed/", "/scripts/DirectNewsTemplate.txt", "newsColumn", 
         "Error reading Direct News feed.");
 
     // NOTE: The following block is provided for testing/review purposes only. It should be
