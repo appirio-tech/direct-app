bdc5126f4cb03079fa6e75001ccf9e191d130154
microwishing <microwishing@fb370eea-3af6-4597-97f7-f7400a59c12a>
2011-09-19 14:40:53 +0000

    delete scorecard_tool
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/applications/direct/trunk@207686 fb370eea-3af6-4597-97f7-f7400a59c12a

--- scorecard_tool/update_roles.sql
+++
@@ -1,8 +0,0 @@
-delete user_role_xref where role_id = 99099;
-delete security_roles where role_id = 99099;
-
-insert into 'informix'.security_roles(role_id, description, create_user_id)
-values('99099', 'Scorecard Administrator', null);
-
-insert into 'informix'.user_role_xref(user_role_id, login_id, role_id, create_user_id, security_status_id)
-values('99099', '132456', '99099', '132456', '1');
