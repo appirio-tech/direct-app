0ac0547c8a5f757d87a5fe82c16881a8b431f4b5
mashannon168 <mashannon168@fb370eea-3af6-4597-97f7-f7400a59c12a>
2013-05-15 19:39:38 +0000

    string compare ignore case.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/applications/direct/trunk@228739 fb370eea-3af6-4597-97f7-f7400a59c12a

--- src/java/main/com/topcoder/direct/services/view/util/DirectUtils.java
+++ src/java/main/com/topcoder/direct/services/view/util/DirectUtils.java
@@ -1727,8 +1727,8 @@ public final class DirectUtils {
         List list = new LinkedList<Map.Entry<Long, String>>(map.entrySet());
         Collections.sort(list, new Comparator() {
             public int compare(Object o1, Object o2) {
-                return ((Comparable) ((Map.Entry) (o1)).getValue())
-                        .compareToIgnoreCase(((Map.Entry) (o2)).getValue());
+                return ((String) ((Map.Entry) (o1)).getValue())
+                        .compareToIgnoreCase((String)((Map.Entry) (o2)).getValue());
             }
         });
 
