8e22e6551d0c3621794ee8267cb451f27df9ac5f
Shannon Ma <sma@appirio.com>
2014-10-15 19:30:47 -0500

    Missing file

---
+++ src/web/scripts/my/challenges.js
@@ -0,0 +1,88 @@
+/**
+ * The javascript for the final panel in the my created challenges and my challenges page.
+ *
+ * Version 1.0 (TopCoder Direct - Challenges Section Filters Panel)
+ *
+ * @author GreatKevin
+ */
+$(document).ready(function () {
+
+    var filters = [
+        "#customerFilter",
+        "#projectFilter",
+        "#challengeStatusFilter",
+        "#challengeTypeFilter"
+    ];
+
+
+    $.each(filters, function (index, value) {
+        sortDropDown(value);
+
+        $(value).change(function () {
+
+            if (value == "#customerFilter") {
+                loadOptionsByClientId($(this).val() == 'All' ? 0 : $(this).val());
+            } else {
+                var table = $.fn.dataTable.fnTables(true);
+
+                if (table.length > 0) {
+                    $(table).dataTable().fnDraw();
+                }
+            }
+
+
+        });
+    });
+
+    function loadOptionsByClientId(clientId) {
+
+        showIndicator($("#projectFilter"));
+
+        $.ajax({
+            type: 'POST',
+            url: "dashboardGetOptionsForClientAJAX",
+            data: {'formData.customerIds': clientId},
+            cache: false,
+            dataType: 'json',
+            async: 'false',
+            success: function (jsonResult) {
+                handleJsonResult(jsonResult,
+                    function (result) {
+                        var $project = $("#projectFilter");
+
+                        $project.html("");
+
+                        if (result.projects) {
+
+                            var projects = result.projects;
+
+                            $.each(projects, function (key, value) {
+                                $project.append($('<option></option>').val(key).html(value));
+                            });
+                        }
+
+                        // append the default "select all"
+                        $project.append($('<option></option>').val("All").html("All Projects"));
+                        $project.val("All");
+
+                        sortDropDown("#projectFilter");
+
+                        hideIndicator($("#projectFilter"));
+
+                        var table = $.fn.dataTable.fnTables(true);
+
+                        if (table.length > 0) {
+                            $(table).dataTable().fnDraw();
+                        }
+
+                    },
+                    function (errorMessage) {
+                        showErrors(errorMessage);
+                        hideIndicator($("#projectFilter"));
+                    });
+            }
+        });
+    }
+
+
+});
