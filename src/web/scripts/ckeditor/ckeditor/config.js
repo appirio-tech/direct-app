fe91b31202bdf9809e514cc08a6eeeb565757e2e
ajefts <ajefts@users.noreply.github.com>
2016-04-19 21:48:00 -0400

    Removed a few more toolbar items

--- src/web/scripts/ckeditor/ckeditor/config.js
+++ src/web/scripts/ckeditor/ckeditor/config.js
@@ -1,22 +1,23 @@
-/**
- * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
- */
-
-CKEDITOR.editorConfig = function (config) {
-    // Define changes to default configuration here. For example:
-    // config.language = 'fr';
-    // config.uiColor = '#AADC6E';
-    config.filebrowserImageUploadUrl = null;
-	config.enterMode = CKEDITOR.ENTER_BR;
-    config.toolbar = [
-        { name: 'document', items : ['Source','Preview','-','Templates','-', 'SpellChecker' ] },
-        { name: 'clipboard', items : [ 'Cut','Copy','Paste','-','Undo','Redo' ] },
-        { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
-        '/',
-        { name: 'styles', items : [ 'Styles','Format','Font','FontSize', '-', 'TextColor','BGColor'] },
-        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote',
-            '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] },
-        { name: 'links', items : [ 'Link','Unlink' ] }
-    ];
-};
+/** 
+ *  * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved. 
+ *   * For licensing, see LICENSE.html or http://ckeditor.com/license 
+ *     
+ */ 
+ 
+CKEDITOR.editorConfig = function (config) { 
+    // Define changes to default configuration here. For example: 
+    // config.language = 'fr'; 
+    // config.uiColor = '#AADC6E'; 
+    config.filebrowserImageUploadUrl = null; 
+    config.enterMode = CKEDITOR.ENTER_BR; 
+    config.toolbar = [ 
+       { name: 'document', items : ['Source','-','Templates','-', 'SpellChecker' ] }, 
+       { name: 'clipboard', items : [ 'Cut','Copy','Paste','-','Undo','Redo' ] }, 
+       { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] }, 
+       '/', 
+       { name: 'styles', items : [ 'Format','Font','FontSize', '-', 'TextColor','BGColor'] }, 
+       { name: 'paragraph', items : [ 'Outdent','Indent','-','Blockquote', 
+       '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] }, 
+       { name: 'links', items : [ 'Link','Unlink' ] } 
+    ]; 
+}; 
