95f9812fb8d9e5153ac386efda1fc493ac820144
Guiqiang Zhang <skyhit@gmail.com>
2016-12-13 12:13:41 +0800

    fix nullpointer exception

--- services/contest_service_facade/src/java/main/com/topcoder/service/facade/contest/ejb/ContestServiceFacadeBean.java
+++ services/contest_service_facade/src/java/main/com/topcoder/service/facade/contest/ejb/ContestServiceFacadeBean.java
@@ -4437,9 +4437,11 @@ public class ContestServiceFacadeBean implements ContestServiceFacadeLocal, Cont
 
                 if (isPrivateProject(contest)){
                     contest.getProjectHeader().setProperty(ProjectPropertyType.RATED_PROJECT_PROPERTY_KEY, "No");
-                }else if (oldProject.getProperty(ProjectPropertyType.PRIVATE_PROJECT).equals("1")){
-                    //remove current registrant
-                    Set<Long> preUsers = updatePreRegister(tcSubject, contest, new HashSet<Long>());
+                }else {
+                    if (oldProject.getProperty(ProjectPropertyType.PRIVATE_PROJECT) != null && oldProject.getProperty(ProjectPropertyType.PRIVATE_PROJECT).equals("1")){
+                        //remove current registrant
+                        Set<Long> preUsers = updatePreRegister(tcSubject, contest, new HashSet<Long>());
+                    }
                 }
 
 
@@ -8976,6 +8978,6 @@ public class ContestServiceFacadeBean implements ContestServiceFacadeLocal, Cont
      * @since 3.4
      */
     private boolean isPrivateProject(SoftwareCompetition contest){
-        return contest.getProjectHeader().getProperty(ProjectPropertyType.PRIVATE_PROJECT).equals("1") ? true :false;
+        return contest.getProjectHeader().getProperty(ProjectPropertyType.PRIVATE_PROJECT) != null && contest.getProjectHeader().getProperty(ProjectPropertyType.PRIVATE_PROJECT).equals("1");
     }
 }
