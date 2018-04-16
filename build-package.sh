67d5ab8e95084e3fb34a66d2f77ca8e04cec67c5
Gunasekar-K <Gunasekar.Kesavalu@wipro.com>
2018-04-16 16:05:43 +0530

    fix for pacakge failure

--- build-package.sh
+++ build-package.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 #Preparing paackage
 VER=`date "+%Y%m%d%H%M"`
 
-function directapp_cdpacakge()
+directapp_cdpacakge()
 {
 #direct app package building
 BUILD_VARIABLE_FILE_NAME="./buildvar-app.conf"
@@ -11,8 +10,9 @@ source $BUILD_VARIABLE_FILE_NAME
 AWS_CD_PACKAGE_NAME="${APPNAME}-${PACKAGETYPE}-${VER}.zip"
 PACAKAGE_LOCATION="dist-${PACKAGETYPE}"
 SCRIPTDIR="./../buildscript/direct/scripts"
+rm -rf $PACAKAGE_LOCATION
 mkdir $PACAKAGE_LOCATION
-cd $PACAKAGE_LOCATION
+#cd $PACAKAGE_LOCATION
 # Take backup of existing direct.jar, direct-static-all.jar and jboss/conf
 cp $SCRIPTDIR/remote-cmd-backup-app.sh $PACAKAGE_LOCATION/
 # Copy direct-static-all.jar and direct.jar to app1 at /home/direct/direct_backup
@@ -22,23 +22,24 @@ cp $SCRIPTDIR/remote-cmd-expand-app.sh $PACAKAGE_LOCATION/
 # Shut down JBoss instance
 cp $SCRIPTDIR/remote-cmd-stop-server.sh $PACAKAGE_LOCATION/
 # Take backup of existing direct.ear and deploy new direct.ear to jboss deploy dir
-cp $SCRIPTDIR/remote-cmd-deploy-app.sh execute $PACAKAGE_LOCATION/
-# start jboss 
-cp $SCRIPTDIR/remote-cmd-start-server.sh execute $PACAKAGE_LOCATION/
+cp $SCRIPTDIR/remote-cmd-deploy-app.sh $PACAKAGE_LOCATION/
+# start jboss
+cp $SCRIPTDIR/remote-cmd-start-server.sh $PACAKAGE_LOCATION/
 # start copying appspec.yml
 cp appspec-app.yml $PACAKAGE_LOCATION/appspec.yml
 zip -j $AWS_CD_PACKAGE_NAME $PACAKAGE_LOCATION/*
 }
 
-function directweb_cdpacakge()
+directweb_cdpacakge()
 {
 #direct app package building
 BUILD_VARIABLE_FILE_NAME="./buildvar-web.conf"
 source $BUILD_VARIABLE_FILE_NAME
 AWS_CD_PACKAGE_NAME="${APPNAME}-${PACKAGETYPE}-${VER}.zip"
 PACAKAGE_LOCATION="dist-${PACKAGETYPE}"
+rm -rf $PACAKAGE_LOCATION
 mkdir $PACAKAGE_LOCATION
-cd $PACAKAGE_LOCATION
+#cd $PACAKAGE_LOCATION
 # Take backup of existing direct.jar, direct-static-all.jar and jboss/conf
 cp $SCRIPTDIR/remote-cmd-backup-web.sh $PACAKAGE_LOCATION/
 # Copy direct-static-all.jar and direct.jar to app1 at /home/direct/direct_backup
@@ -46,7 +47,7 @@ cp direct-static-all.jar $PACAKAGE_LOCATION/
 # Unzip direct-static-all.jar and move ccs, js and images to /usr/local/apache/tcdocs /css/scrits/images
 cp $SCRIPTDIR/remote-cmd-expand-web.sh $PACAKAGE_LOCATION/
 # start copying appspec.yml
-cp appspec-app.yml $PACAKAGE_LOCATION/appspec.yml
+cp appspec-web.yml $PACAKAGE_LOCATION/appspec.yml
 zip -j $AWS_CD_PACKAGE_NAME $PACAKAGE_LOCATION/*
 }
 
@@ -55,4 +56,5 @@ directapp_cdpacakge
 directweb_cdpacakge
 VER1=$VER
 echo export VER="$VER1" >> "$BASH_ENV"
+
 
---
+++ deploy.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+APPVER=$1
+
+if [ -z $APPVER ];
+then
+ log "The script need to be executed with version ex:deploy.sh 123"
+ exit 1
+fi
+
+BUILD_VARIABLE_FILE_NAME="./buildvar.conf"
+source $BUILD_VARIABLE_FILE_NAME
+
+if [ "$DEPLOY" = "1" ] ;
+then
+    echo "build variables are imported. Proceeding deployment"
+else
+    log "User skipped deployment by updating the DEPLOY variable other than 1"
+    exit 1
+fi
+
+SECRET_FILE_NAME="${APPNAME}-buildsecvar.conf"
+cp ./../buildscript/${APPNAME}/${SECRET_FILE_NAME}.enc .
+if [ -f "$SECRET_FILE_NAME" ];
+then
+   rm -rf $SECRET_FILE_NAME
+fi
+openssl enc -aes-256-cbc -d -in $SECRET_FILE_NAME.enc -out $SECRET_FILE_NAME -k $SECPASSWD
+source $SECRET_FILE_NAME
+
+AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
+AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
+AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
+AWS_CD_APPNAME=$(eval "echo \$${ENV}_AWS_CD_APPNAME")
+AWS_CD_DG_NAME=$(eval "echo \$${ENV}_AWS_CD_DG_NAME")
+AWS_CD_DG_CONFIGURATION=$(eval "echo \$${ENV}_AWS_CD_DG_CONFIGURATION")
+AWS_S3_BUCKET=$(eval "echo \$${ENV}_AWS_S3_BUCKET")
+AWS_S3_KEY_LOCATION=$(eval "echo \$${ENV}_AWS_S3_KEY_LOCATION")
+
+AWS_CD_PACKAGE_NAME="${APPNAME}-${PACKAGETYPE}-${APPVER}.zip"
+if [ "$AWS_S3_KEY_LOCATION" = "" ] ;
+then
+    AWS_S3_KEY="${AWS_CD_PACKAGE_NAME}"
+else
+    AWS_S3_KEY="$AWS_S3_KEY_LOCATION/${AWS_CD_PACKAGE_NAME}"
+fi
+
+#log Function - Used to provide information of execution information with date and time
+log()
+{
+   echo "`date +'%D %T'` : $1"
+}
+
+#track_error function validates whether the application execute without any error
+track_error()
+{
+   if [ $1 != "0" ]; then
+        log "$2 exited with error code $1"
+        log "completed execution IN ERROR at `date`"
+        exit $1
+   fi
+
+}
+
+#Function for aws login
+configure_aws_cli() {
+	aws --version
+	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+	aws configure set default.region $AWS_REGION
+	aws configure set default.output json
+	log "Configured AWS CLI."
+}
+
+
+#uploading to S3 bucket
+upload_cd_pakcage()
+{
+    S3_URL=""
+	if [ "$AWS_S3_KEY_LOCATION" = "" ] ;
+	then
+		S3_URL="s3://${AWS_S3_BUCKET}/"
+	else
+		S3_URL="s3://${AWS_S3_BUCKET}/${AWS_S3_KEY_LOCATION}/"
+	fi
+	aws s3 cp ${AWS_CD_PACKAGE_NAME} $S3_URL
+	track_error $? "Package S3 deployment"
+	log "CD Package uploaded successfully to S3 bucket $S3_URL"
+}
+#register the revision in Code deploy
+update_cd_app_revision()
+{
+	aws deploy register-application-revision --application-name "${AWS_CD_APPNAME}" --s3-location bucket=${AWS_S3_BUCKET},bundleType=zip,key=${AWS_S3_KEY}
+	track_error $? "CD applicaton register"
+	log "CD application register completed successfully"
+}
+#Invoke the code deploy
+cd_deploy()
+{
+	$DEPLOYID=`aws deploy create-deployment --application-name "${AWS_CD_APPNAME}" --deployment-config-name ${AWS_CD_DG_CONFIGURATION} --deployment-group-name ${AWS_CD_DG_NAME} --s3-location bucket=code-deploy-hello,bundleType=zip,key=${AWS_S3_KEY}`
+	track_error $? "CD applicaton register"
+	log "CD application register completed successfully. Please find the $DEPLOYID"
+}
+#Checing the status
+cd_deploy_status()
+{
+	echo "check statusget info aws deploy get-deployment --deployment-id d-USUAELQEX"
+	
+}
+configure_aws_cli
+upload_cd_pakcage
+#update_cd_app_revision
+#cd_deploy
+#cd_deploy_status
+
+
