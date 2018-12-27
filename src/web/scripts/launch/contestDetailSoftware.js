5f9757c277bc31240d5ac9748e7f8bcc447b7495
ajefts <ajefts@users.noreply.github.com>
2018-12-27 14:42:48 -0500

    trying to add prize levels to Code challenge type

--- src/web/scripts/launch/contestDetailSoftware.js
+++ src/web/scripts/launch/contestDetailSoftware.js
@@ -958,7 +958,7 @@ function initContest(contestJson) {
      }
      if (!hasContestPrize) {
        projectHeader.prizes.push(new com.topcoder.direct.Prize(1, 0, CONTEST_PRIZE_TYPE_ID, 1));
-       if(!(isDesignF2F() || isF2F() || isBugHunt() || isCode())) {
+       if(!(isDesignF2F() || isF2F() || isBugHunt())) {
          projectHeader.prizes.push(new com.topcoder.direct.Prize(2, 0, CONTEST_PRIZE_TYPE_ID, 1));
        }
      }
